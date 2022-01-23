c46b66e407e8853c4cc66b12d207b3055233f21d
Jorshyy <jorshyy19367@gmail.com>
2022-01-22 17:00:10 -0700

    first commit, add array of quotes

---
+++ main.js
@@ -0,0 +1,15 @@
+// This program generates quotes randomy for a user who is needing motivation in their life
+
+const quote = ["If opportunity doesn't knock, build a door -Milton Berle", 
+"“The secret of success is to do the common thing uncommonly well.” -John D. Rockefeller Jr.", 
+"Be uncommon amongst uncommon people. David Goggins", 
+"“Where there is no struggle, there is no strength.” -Oprah Winfrey",
+"“Only those who are asleep make no mistakes.” -Ingvar Kamprad", 
+"“If your actions inspire others to dream more, learn more, do more and become more, you are a leader.” -John Quincy Adams",
+"“The fastest way to change yourself is to hang out with people who are already the way you want to be.” -Reid Hoffman",
+"Hard Work Beats Talent When Talent Doesn't Work Hard -Tim Notke"]
+
+
+console.log(quote.length);
+
+
