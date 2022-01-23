4ef7052a90b2fdadb2ba8a0722549975c900b96e
Jorshyy <jorshyy19367@gmail.com>
2022-01-22 17:11:43 -0700

    add a function to generate random quote

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// This program generates quotes randomy for a user who is needing motivation in their life
+// This program generates quotes randomly for a user who is needing motivation in their life
 
 const quote = ["If opportunity doesn't knock, build a door -Milton Berle", 
 "“The secret of success is to do the common thing uncommonly well.” -John D. Rockefeller Jr.", 
@@ -7,9 +7,17 @@ const quote = ["If opportunity doesn't knock, build a door -Milton Berle",
 "“Only those who are asleep make no mistakes.” -Ingvar Kamprad", 
 "“If your actions inspire others to dream more, learn more, do more and become more, you are a leader.” -John Quincy Adams",
 "“The fastest way to change yourself is to hang out with people who are already the way you want to be.” -Reid Hoffman",
-"Hard Work Beats Talent When Talent Doesn't Work Hard -Tim Notke"]
+"Hard Work Beats Talent When Talent Doesn't Work Hard -Tim Notke"];
 
 
-console.log(quote.length);
+// generates random quote using the quote array
+const quoteGenerator = arr => {
+    let random = Math.floor(Math.random() * (arr.length - 1));
+    return quote[random];
+    };
+
+
+
+console.log(quoteGenerator(quote));
 
 
